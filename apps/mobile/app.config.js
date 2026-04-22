931b85b7499db3fafb030943d206158c85c30b9c
Mohit gauniyal <mohitgauniyal19a@gmail.com>
2026-04-22 21:23:14 +0530

    config: migrate app.json to app.config.js with dynamic GOOGLE_SERVICES_JSON env var

---
+++ apps/mobile/app.config.js
@@ -0,0 +1,38 @@
+export default {
+  expo: {
+    name: "Vaastio",
+    slug: "vaastio",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "light",
+    splash: {
+      image: "./assets/splash.png",
+      resizeMode: "cover",
+      backgroundColor: "#4338ca"
+    },
+    ios: {
+      supportsTablet: true
+    },
+    android: {
+      googleServicesFile: process.env.GOOGLE_SERVICES_JSON ?? "./google-services.json",
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#4338ca"
+      },
+      package: "com.inspirebyte.vaastio",
+      edgeToEdgeEnabled: false,
+      predictiveBackGestureEnabled: false
+    },
+    web: {
+      favicon: "./assets/favicon.png"
+    },
+    plugins: ["expo-font"],
+    extra: {
+      eas: {
+        projectId: "482ccdae-8d10-49d6-b9c6-cd140036a1b0"
+      }
+    },
+    owner: "mohitgauniyal"
+  }
+}
